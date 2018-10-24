0c8961d84f6f63253a7fd3ae8e93220faca166e4
studart301d <gabrielstudart@alu.ufc.br>
2018-10-24 04:28:28 -0300

    Dia 1

---
+++ Pratica2/Pratica2.ino
@@ -0,0 +1,19 @@
+//led no pino A5m
+//led no pino 9
+
+// Ler o map
+
+void setup() {
+
+  //Pino do led será saída
+ //Pino com potenciômetro será entrada
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // map
+  // escreve no pino led o valor da leitura.
+
+}
---
+++ praticas_finalizadas/Pratica2/Pratica2.ino
@@ -0,0 +1,20 @@
+const int pot = A5; //led no pino A5m
+const int led = 9; //led no pino 9
+
+int leitura; // Ler o pot
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(led,OUTPUT);//Pino do led será saída
+  pinMode(pot,INPUT);//Pino com potenciômetro será entrada
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  leitura = map(analogRead(pot),0,1023,0,255); // map.
+  analogWrite(led,leitura); // escreve no pino led o valor da leitura.
+
+}
