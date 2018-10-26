6de8895fd6e3053124a73d3450342700f6b50ac3
studart301d <gabrielstudart@alu.ufc.br>
2018-10-26 06:47:43 -0300

    add distance fuction

--- Pratica5/Pratica5.ino
+++ Pratica5/Pratica5.ino
@@ -7,3 +7,19 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+ float distance() {
+  
+          digitalWrite(trigPin, LOW);
+          delayMicroseconds(2);
+
+          digitalWrite(trigPin, HIGH);
+
+          delayMicroseconds(10);
+
+          digitalWrite(echoPin,LOW);
+
+          float dist = pulseIn(echoPin,HIGH)*0.034/2 ;
+        
+          return dist;
+        }
